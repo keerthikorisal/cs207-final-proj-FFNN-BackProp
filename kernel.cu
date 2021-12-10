082691d6e569bc00fc5356a2921f1e8371eb39b8
mchoi041 <38056099+mchoi041@users.noreply.github.com>
2021-12-10 05:33:57 -0800

    Add files via upload

--- kernel.cu
+++ kernel.cu
@@ -14,27 +14,17 @@ __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, flo
      *   where A is a (m x k) matrix
      *   where B is a (k x n) matrix
      *   where C is a (m x n) matrix
-     *
-     * Use shared memory for tiling
-     *
-     ********************************************************************/
-
-    /*************************************************************************/
-    // INSERT KERNEL CODE HERE
+     *******************************************************************/
 	__shared__ float ds_A[TILE_SIZE][TILE_SIZE];
 	__shared__ float ds_B[TILE_SIZE][TILE_SIZE];
-
 	int bx = blockIdx.x;
 	int by = blockIdx.y;
         int tx = threadIdx.x;
 	int ty = threadIdx.y;
-
 	int row = by * blockDim.y + ty;
 	int col = bx * blockDim.x + tx;
 	float pvalue = 0;
-
 	for ( int p = 0; p < (k - 1)/TILE_SIZE+1; p++){
-
 		if((p*TILE_SIZE + tx < k) && (row < m)){
 			ds_A[ty][tx] = A[row * k + p * TILE_SIZE+tx];
 		}
@@ -56,32 +46,16 @@ __global__ void mysgemm(int m, int n, int k, const float *A, const float *B, flo
 		if(row < m && col < n){
 			C[row * n + col] = pvalue;
 		}
-
 	}
-    /*************************************************************************/
 }
 
 void basicSgemm(int m, int n, int k, const float *A, const float *B, float *C)
 {
-    // Initialize thread block and kernel grid dimensions ---------------------
-
     const unsigned int BLOCK_SIZE = TILE_SIZE;
-	
-    /*************************************************************************/
-    //INSERT CODE HERE
-	dim3 dimGrid((n-1)/BLOCK_SIZE + 1, (m-1) / BLOCK_SIZE +1, 1);
-	dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+    dim3 dimGrid((n-1)/BLOCK_SIZE + 1, (m-1) / BLOCK_SIZE +1, 1);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
  
-    /*************************************************************************/
-
-    // Invoke CUDA kernel -----------------------------------------------------
-
-    /*************************************************************************/
-    //INSERT CODE HERE
-	mysgemm <<< dimGrid, dimBlock >>> (m, n, k, A, B, C);	
-    /*************************************************************************/
-
-
+    mysgemm <<< dimGrid, dimBlock >>> (m, n, k, A, B, C);	
 }
 
 __device__ __forceinline__ float sigmoid (float a){
@@ -108,114 +82,18 @@ void basicSigmoid(const float *C, float  *Csig, int len){
 }
 
 
-__global__ void mysub(int m, const float *A, const float *B, float* C){
-
-	// INSERT KERNEL CODE HERE
-        //__shared__ float ds_A[TILE_SIZE][TILE_SIZE];
-        //__shared__ float ds_B[TILE_SIZE][TILE_SIZE];
-
-        int bx = blockIdx.x;
-        int by = blockIdx.y;
-	/*
-        int tx = threadIdx.x;
-        int ty = threadIdx.y;
-
-        int row = by * blockDim.y + ty;
-        int col = bx * blockDim.x + tx;
-        //float pvalue = 0;
-
-       * for (int p = 0; p < (k - 1)/TILE_SIZE+1; p++){
-
-                if(row < m && col < n){
-                        for (int i = 0; i <  TILE_SIZE; ++i){
-                                pvalue = ds_A[ty][i] - ds_B[ty][i];
-                        }
-                }
-                __syncthreads();
-
-                if(row < m && col < n){
-                        C[row * n + col] = pvalue;
-                }
-
-        }*/
-	int id = gridDim.x * by + bx;
-	for(int i = 0; i < m; i ++){
-		C[i] = A[i] - B[i];
+__global__ void mysub(int m,int n, int k, const float *A, const float *B, float* C){
+	for (int i = 0; i < (m * n); i++) {
+		C[i] = A[i] - B[i];	
 	}
-	/*
-	int colID = threadIdx.x + blockIdx.x * blockDim.x;	// Row address
-
-	if(colID < n) {
-		for(int i = 0; i<n; i++){
-			//elemID = colID + rowID * WIDTH; 
-			C[colID + i*n] = A[colID + i*n] + B[colID + i*n];
-		}
-	}*/
 }
 
-void basicSub(int m, const float *A, const float *B, float* C){
-	// Initialize thread block and kernel grid dimensions ---------------------
-
+void basicSub(int m,int n, int k, const float *A, const float *B, float* C){
     const unsigned int BLOCK_SIZE = TILE_SIZE;
 
-    /*************************************************************************/
-    //INSERT CODE HERE
-        dim3 dimGrid((m-1)/BLOCK_SIZE + 1, (m - 1) / BLOCK_SIZE + 1, 1);
-        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
+    dim3 dimGrid((n-1)/BLOCK_SIZE + 1, (m-1) / BLOCK_SIZE + 1, 1);
+    dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
 
-    /*************************************************************************/
-
-    // Invoke CUDA kernel -----------------------------------------------------
-
-    /*************************************************************************/
-    //INSERT CODE HERE
-        mysub <<< dimGrid, dimBlock >>> (m, A, B, C);
-    /*************************************************************************/
-
-
+    mysub <<< dimGrid, dimBlock >>> (m,n, k, A, B, C);
 }
 
-__global__ void mytrans(int m, int n, const float *OW_d, float *OW_t_d){
-	__shared__ float tile[TILE_SIZE][TILE_SIZE];
-
-	int x = blockIdx.x * TILE_SIZE + threadIdx.x;
-	int y = blockIdx.y * TILE_SIZE + threadIdx.y;
- 	if((x < m) && (y < n))
-	{
-		unsigned int index_in = y * m + x;
-		tile[threadIdx.y][threadIdx.x] = OW_d[index_in];
-	}
-	__syncthreads();
-
-	x = blockIdx.y * TILE_SIZE + threadIdx.x;
-	y = blockIdx.x * TILE_SIZE + threadIdx.y;
-	if((x < n) && (y < m))
-	{
-		unsigned int index_out = y * n + x;
-		OW_t_d[index_out] = tile[threadIdx.x][threadIdx.y];
-	}
-
-}
-
-void basicTrans(int m, int n, const float *OW_d, float *OW_t_d){
-	
-	// Initialize thread block and kernel grid dimensions ---------------------
-
-    const unsigned int BLOCK_SIZE = TILE_SIZE;
-
-    /*************************************************************************/
-    //INSERT CODE HERE
-        dim3 dimGrid(m / BLOCK_SIZE, n / BLOCK_SIZE, 1);
-        dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
-
-    /*************************************************************************/
-
-    // Invoke CUDA kernel -----------------------------------------------------
-
-    /*************************************************************************/
-    //INSERT CODE HERE
-        mytrans <<< dimGrid, dimBlock >>> (m, n, OW_d, OW_t_d);
-    /*************************************************************************/
-			
-
-}
--- main.cu
+++ main.cu
@@ -3,91 +3,17 @@
 #include "kernel.cu"
 #include "support.h"
 #include <iostream>
-#include <cublas_v2.h>
 
-
-void cublas_sgemm(int m, int n, int k, const float *A, const float *B, float *C){
-	int lda=m,ldb=k,ldc=m;
-        const float alf = 1;
-        const float bet = 0;
-        const float *alpha = &alf;
-        const float *beta = &bet;
-
-        // Create a handle for CUBLAS
-        cublasHandle_t handle;
-        cublasCreate(&handle);
-
-        // Do the actual multiplication
-        cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, alpha, B, ldb, A, lda, beta, C, ldc);
-
-        // Destroy the handle
-        cublasDestroy(handle);
-
-}
-
-void cublas_out_sgemm(int m, int n, int k, const float *OW, float *C, float *OUT){
-        int lda=m,ldb=k,ldc=m;
-        const float alf = 1;
-        const float bet = 0;
-        const float *alpha = &alf;
-        const float *beta = &bet;
-
-        // Create a handle for CUBLAS
-        cublasHandle_t handle;
-        cublasCreate(&handle);
-
-        // Do the actual multiplication
-        cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, n, m, k, alpha, C, ldb, OW, lda, beta, OUT, ldc);
-
-        // Destroy the handle
-        cublasDestroy(handle);
-
-}
-
-void cublas_transpose(int m, int n, const float *OW_d, float *OW_t_d){
-	float alpha=1;
-	const float *A = OW_d;
-        int lda=n;
-
-        float beta=0;
-	float *B = NULL;
-        int ldb=n;
-
-        float *C=OW_t_d;
-        int ldc=m;
-
-        cublasHandle_t handle;
-        cublasCreate(&handle);
-        cublasStatus_t success=cublasSgeam( handle, CUBLAS_OP_T, CUBLAS_OP_N, m, n, &alpha, A, lda, &beta, B, ldb, C, ldc);
-	OW_t_d = C;
-        if ( success != CUBLAS_STATUS_SUCCESS)
-           // cout << "\33[31mError: " << success << "\33[0m\n";
-        cublasDestroy(handle);
-
-} 
-
-void feed_forward(unsigned matArow, unsigned matBcol, unsigned matBrow, int len, int OUT_len, float *OW_d, float *C_d, float *Csig_d, float *OUT_d, float *A_d, float *B_d, Timer timer, cudaError_t cuda_ret){
-
-	// Launch kernel using standard sgemm interface ---------------------------
-	printf("Launching kernel..."); fflush(stdout);
-    	startTime(&timer);
-    	//Hidden Layer Calculation
-    	basicSgemm(matArow, matBcol, matBrow, A_d, B_d, C_d);
-	printf("\nFUck");
-    	//cublas_sgemm(matArow, matBcol, matBrow, A_d, B_d, C_d);
+void feed_forward(unsigned matArow, unsigned matBcol, unsigned matBrow, int len, int OUT_len, float *OW_d, float *C_d, float *Csig_d, float *OUT_d, float *A_d, float *B_d, cudaError_t cuda_ret){
+    	cuda_ret = cudaDeviceSynchronize();
+	basicSgemm(matArow, matBcol, matBrow, A_d, B_d, C_d);
     	cuda_ret = cudaDeviceSynchronize();
     	basicSigmoid(C_d, Csig_d, len);
     	cuda_ret = cudaDeviceSynchronize();
-
-    	//Output Layer Calcultion
     	basicSgemm(matBcol, matBcol, matArow, OW_d, Csig_d, OUT_d);
-    	//cublas_out_sgemm(matBcol, matBcol, matArow, OW_d, C_d, OUT_d);
     	cuda_ret = cudaDeviceSynchronize();
     	basicSigmoid(OUT_d, OUT_d,  OUT_len);
     	cuda_ret = cudaDeviceSynchronize();
-	printf("\nMFFFF");
-    	if(cuda_ret != cudaSuccess) printf("Unable to launch kernel");
-    	stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 }
 
 void back_prop_output(const float *OW_d, float *C_d, float *OW_new_d, float *update_weight_d, float *OW_t_d,  cudaError_t cuda_ret){
@@ -96,13 +22,9 @@ void back_prop_output(const float *OW_d, float *C_d, float *OW_new_d, float *upd
 	cuda_ret = cudaDeviceSynchronize();
 	basicSgemm(matBrow, matBcol, matArow, C_d, update_weight_d, OW_new_d);
 	cuda_ret = cudaDeviceSynchronize();
-	//basicTrans(matBrow, matBcol, OW_new_d, OW_t_d);
-	//cubals_transpose(matBcol, matBrow, OW_d, OW_t_d);
 	cuda_ret = cudaDeviceSynchronize();
-	basicSub(matBrow*matArow, OW_d, OW_new_d, OW_new_d);
+	basicSub(matBcol, matBrow, matArow, OW_d, OW_new_d, OW_new_d);
 	cuda_ret = cudaDeviceSynchronize();
-	//if(cuda_ret != cudaSuccess) printf("Unable to launch kernel");
-        //stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 }
 
 void back_prop_hidden(float *B_d, float *update_weight_d, float *A_d, float *OW_d, float *OW_t_d, float *B_new_d, float *B_temp_d, cudaError_t cuda_ret){
@@ -111,16 +33,12 @@ void back_prop_hidden(float *B_d, float *update_weight_d, float *A_d, float *OW_
         cuda_ret = cudaDeviceSynchronize();
 	basicSgemm(matArow, matBcol, matBcol, B_d, update_weight_d, B_temp_d);
 	cuda_ret = cudaDeviceSynchronize();
-	//transpose
-	//basicTrans(matBrow, matBcol, OW_d, OW_t_d);
-	cublas_transpose(matBcol, matBrow, OW_d, OW_t_d);
-	//printf("\ntrans: %f/%f", OW_h, OW_t_h); 
-	cuda_ret = cudaDeviceSynchronize();
-	basicSgemm(matArow, matBrow, matBcol, B_temp_d, OW_t_d, B_new_d);
+	basicSgemm(matArow, matBrow, matBcol, B_temp_d, OW_d,  B_new_d);
         cuda_ret = cudaDeviceSynchronize();
-	//basicSub(matBrow*matArow, A_d, B_new_d, B_new_d);
-        //cuda_ret = cudaDeviceSynchronize();
-} 
+	basicSub(matBrow, matArow, matBcol, A_d, B_new_d, B_new_d);
+	cuda_ret = cudaDeviceSynchronize();
+}
+ 
 
 int main (int argc, char *argv[])
 {
@@ -132,7 +50,7 @@ int main (int argc, char *argv[])
 
     printf("\nSetting up the problem..."); fflush(stdout);
     startTime(&timer);
-
+    
     float *A_h, *B_h, *C_h, *Csig_h, *OW_h, *OUT_h, *OW_new_h, *update_weight_h, *OW_t_h, *B_new_h, *B_temp_h;
     float *A_d, *B_d, *C_d, *Csig_d, *OW_d, *OUT_d, *OW_new_d, *update_weight_d, *OW_t_d, *B_new_d, *B_temp_d;
     size_t A_sz, B_sz, C_sz, Csig_sz, OW_sz, OUT_sz, OW_new_sz, update_sz, OW_t_sz, B_new_sz, B_temp_sz;
@@ -161,26 +79,27 @@ int main (int argc, char *argv[])
       "\n");
         exit(0);
     }
-   
-    A_sz = matArow*matAcol; //weights (5x4)
-    B_sz = matBrow*matBcol; //inputs (4x1)
-    C_sz = matArow*matBcol; //hidden output (5x1)
-    Csig_sz = C_sz; // sigmoid hidden
-    OW_sz = matArow*matBcol; //output weights (5x1)
-    OUT_sz = matBcol; //output 
-    len = C_sz; 
+    
+    //Matrix sizes
+    A_sz = matArow*matAcol;
+    B_sz = matBrow*matBcol;
+    C_sz = matArow*matBcol;
+    Csig_sz = C_sz;
+    OW_sz = matArow*matBcol;
+    OUT_sz = matBcol;
+    len = C_sz;
     OUT_len = OUT_sz;
-	OW_new_sz = OW_sz; //BP output weights (4x1)
-	update_sz = OUT_sz;
-	OW_t_sz = OW_sz;
-	B_new_sz = A_sz;
-	B_temp_sz = B_sz;
+    OW_new_sz = OW_sz;
+    update_sz = OUT_sz;
+    OW_t_sz = A_sz;
+    B_new_sz = A_sz;
+    B_temp_sz = B_sz;
 
     A_h = (float*) malloc( sizeof(float)*A_sz );
     for (unsigned int i=0; i < A_sz; i++) { A_h[i] = (rand()%100)/100.00; }
 
     B_h = (float*) malloc( sizeof(float)*B_sz );
-    for (unsigned int i=0; i < B_sz; i++) { B_h[i] = (rand()%100)/100.00; printf("\nBlah: %f", B_h[i]); }
+    for (unsigned int i=0; i < B_sz; i++) { B_h[i] = (rand()%100)/100.00; }
 
     Csig_h = (float*) malloc( sizeof(float)*Csig_sz);
     for (unsigned int i = 0; i < Csig_sz; i++) { Csig_h[i] = (rand()%100)/100.00; }
@@ -189,18 +108,12 @@ int main (int argc, char *argv[])
     for(unsigned int i = 0; i < OW_sz; i++) { OW_h[i] = (rand()%100)/100.00; }
 
     C_h = (float*) malloc( sizeof(float)*C_sz );
-
     OUT_h = (float*) malloc(sizeof(float)*OUT_sz);
-
-	OW_new_h = (float*) malloc(sizeof(float)*OW_new_sz);
-
-	update_weight_h = (float*) malloc(sizeof(float)*update_sz);
-	
-	OW_t_h = (float*) malloc(sizeof(float)*OW_t_sz);
-	
-	B_new_h = (float*) malloc( sizeof(float)*B_new_sz);
-
-	B_temp_h = (float*) malloc( sizeof(float)*B_temp_sz );
+    OW_new_h = (float*) malloc(sizeof(float)*OW_new_sz);
+    update_weight_h = (float*) malloc(sizeof(float)*update_sz);	
+    OW_t_h = (float*) malloc(sizeof(float)*OW_t_sz);	
+    B_new_h = (float*) malloc( sizeof(float)*B_new_sz);
+    B_temp_h = (float*) malloc( sizeof(float)*B_temp_sz );
 
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
     printf("    A: %u x %u\n    B: %u x %u\n    C: %u x %u\n", matArow, matAcol,
@@ -211,129 +124,120 @@ int main (int argc, char *argv[])
     printf("Allocating device variables..."); fflush(stdout);
     startTime(&timer);
 
-    /*************************************************************************/
-    //INSERT CODE HERE
-	cudaMalloc((void**) &A_d, sizeof(float)*A_sz);
-	cudaMalloc((void**) &B_d, sizeof(float)*B_sz);
-	cudaMalloc((void**) &C_d, sizeof(float)*C_sz);
-	cudaMalloc((void**) &OW_d, sizeof(float)*OW_sz);
-	cudaMalloc((void**) &OUT_d, sizeof(float)*OUT_sz);
-	cudaMalloc((void**) &Csig_d, sizeof(float)*Csig_sz);
-	cudaMalloc((void**) &OW_new_d, sizeof(float)*OW_new_sz);
-	cudaMalloc((void**) &update_weight_d, sizeof(float)*update_sz);
-	cudaMalloc((void**) &OW_t_d, sizeof(float)*OW_t_sz);
-	cudaMalloc((void**) &B_new_d, sizeof(float)*B_new_sz);
-	cudaMalloc((void**) &B_temp_d, sizeof(float)*B_temp_sz);
-    /*************************************************************************/
-	
+    cudaMalloc((void**) &A_d, sizeof(float)*A_sz);
+    cudaMalloc((void**) &B_d, sizeof(float)*B_sz);
+    cudaMalloc((void**) &C_d, sizeof(float)*C_sz);
+    cudaMalloc((void**) &OW_d, sizeof(float)*OW_sz);
+    cudaMalloc((void**) &OUT_d, sizeof(float)*OUT_sz);
+    cudaMalloc((void**) &Csig_d, sizeof(float)*Csig_sz);
+    cudaMalloc((void**) &OW_new_d, sizeof(float)*OW_new_sz);
+    cudaMalloc((void**) &update_weight_d, sizeof(float)*update_sz);
+    cudaMalloc((void**) &OW_t_d, sizeof(float)*OW_t_sz);
+    cudaMalloc((void**) &B_new_d, sizeof(float)*B_new_sz);
+    cudaMalloc((void**) &B_temp_d, sizeof(float)*B_temp_sz);
+    /*************************************************************************/	
+
     cudaDeviceSynchronize();
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
-
     // Copy host variables to device ------------------------------------------
+
     printf("Copying data from host to device..."); fflush(stdout);
     startTime(&timer);
 	
     /*************************************************************************/
-    //INSERT CODE HERE
-	cudaMemcpy(A_d, A_h, sizeof(float)*A_sz, cudaMemcpyHostToDevice);
-	cudaMemcpy(B_d, B_h, sizeof(float)*B_sz, cudaMemcpyHostToDevice);
-	cudaMemcpy(OW_d, OW_h, sizeof(float)*OW_sz, cudaMemcpyHostToDevice);
+    //Host to Device
+    cudaMemcpy(A_d, A_h, sizeof(float)*A_sz, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B_h, sizeof(float)*B_sz, cudaMemcpyHostToDevice);
+    cudaMemcpy(OW_d, OW_h, sizeof(float)*OW_sz, cudaMemcpyHostToDevice);
     /*************************************************************************/
     
     cudaDeviceSynchronize();
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 
-	// Feed_forward function ----------------------------------------
-	feed_forward(matArow, matBcol, matBrow, len, OUT_len, OW_d, C_d, Csig_d, OUT_d, A_d, B_d, timer, cuda_ret);
+    // Feed_forward function --------------------------------------------------
+    feed_forward(matArow, matBcol, matBrow, len, OUT_len, OW_d, C_d, Csig_d, OUT_d, A_d, B_d, cuda_ret);
 
-	/*/ back_prop_output
-	//printf("\nOUT_D: %f", OUT_d[0]);
-	update_weight_h[0] = (OUT_d[0] - 1.0f)*0.05f;
-	cudaMemcpy(update_weight_d, update_weight_h, sizeof(float)*update_sz, cudaMemcpyHostToDevice); 
-	//= (OUT_d[0] - 1.0f)*0.05f;
-	//printf("\nupdate: %f", update_weight_d[0]);
-	//const float activation = 0.05;
-	back_prop_output(OW_d, C_d, OW_new_d, update_weight_d, OW_new_sz, cuda_ret);
-	printf("\nOW_new: %f", OW_new_d[0]);
-	printf("\nOW_orig: %f", OW_d[0]);
-	// --------------------------------------------------------------*/
 
     // Copy device variables from host ----------------------------------------
-    printf("Copying data from device to host..."); fflush(stdout);
-    startTime(&timer);
+    //printf("Copying data from device to host..."); fflush(stdout);
+    //startTime(&timer);
 
     /*************************************************************************/
-    //INSERT CODE HERE
-	cudaMemcpy(C_h, C_d, sizeof(float)*C_sz, cudaMemcpyDeviceToHost);
-	cudaMemcpy(Csig_h, Csig_d, sizeof(float)*Csig_sz, cudaMemcpyDeviceToHost);
-	cudaMemcpy(OUT_h, OUT_d, sizeof(float)*OUT_sz, cudaMemcpyDeviceToHost);
-	//cudaMemcpy(OW_new_h, OW_new_d, sizeof(float)*OW_new_sz, cudaMemcpyDeviceToHost);
+    //Device To Host
+    cudaMemcpy(C_h, C_d, sizeof(float)*C_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(Csig_h, Csig_d, sizeof(float)*Csig_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(OUT_h, OUT_d, sizeof(float)*OUT_sz, cudaMemcpyDeviceToHost);
     /*************************************************************************/
 
     cudaDeviceSynchronize();
     stopTime(&timer); printf("%f s\n", elapsedTime(timer));
 	
-	// back_prop_output
-        //printf("\nOUT_D: %f", OUT_d[0]);
-        update_weight_h[0] = (OUT_h[0] - 1.0f)*0.05f;
-        cudaMemcpy(update_weight_d, update_weight_h, sizeof(float)*update_sz, cudaMemcpyHostToDevice);
-        //= (OUT_d[0] - 1.0f)*0.05f;
-        printf("\nupdate: %f", update_weight_h[0]);
-        //const float activation = 0.05;
-        back_prop_output(OW_d, C_d, OW_new_d, update_weight_d, OW_t_d, cuda_ret);
-        //printf("\nOW_new: %f", OW_new_h[0]);
-        printf("\nOW_orig: %f", OW_h[1]);
-	back_prop_hidden(B_d, update_weight_d, A_d, OW_d, OW_t_d, B_new_d, B_temp_d, cuda_ret);
-
-	cudaMemcpy(OW_new_h, OW_new_d, sizeof(float)*OW_new_sz, cudaMemcpyDeviceToHost);
-	cudaMemcpy(OW_h, OW_d, sizeof(float)*OW_sz, cudaMemcpyDeviceToHost);
-	cudaMemcpy(OW_t_h, OW_t_d, sizeof(float)*OW_t_sz, cudaMemcpyDeviceToHost);
-	cudaMemcpy(B_new_h, B_new_d, sizeof(float)*B_new_sz, cudaMemcpyDeviceToHost);
+    //Back_prop_output---------------------------------------------------------
+    update_weight_h[0] = (OUT_h[0] - 1.0f) * 0.05f;
+    cudaMemcpy(update_weight_d, update_weight_h, sizeof(float)*update_sz, cudaMemcpyHostToDevice);
+    
+    printf("\nupdate: %f", update_weight_h[0]);
+       
+    back_prop_output(OW_d, C_d, OW_new_d, update_weight_d, OW_t_d, cuda_ret);
+    
+    printf("\nOW_orig: %f", OW_h[1]);
 	
-	 //printf("\nOW_t: %f/%f/%f/%f/%f", OW_t_h[0],OW_t_h[1],OW_t_h[2],OW_t_h[3],OW_t_h[4]);
-	printf("\nOW_new: %f/%f/%f/%f/%f", OW_new_h[0],OW_new_h[1],OW_new_h[2],OW_new_h[3],OW_new_h[4]);
-	printf("\nOW_curr: %f/%f/%f/%f/%f", OW_h[0],OW_h[1],OW_h[2],OW_h[3],OW_h[4]);
-	for(int i = 0; i < 10; i++){
-		printf("\nWOW/OW_t %f/%f",OW_h[i], OW_t_h[i]);
-	}
-	printf("\nNew_b: %f/%f", B_h[1], B_new_h[1]);
-
+    back_prop_hidden(B_d, update_weight_d, A_d, OW_d, OW_t_d, B_new_d, B_temp_d, cuda_ret);
+ /*   
+    for(int i = 0; i < 100; i++) {
+	feed_forward(matArow, matBcol, matBrow, len, OUT_len, OW_new_d, C_d, Csig_d, OUT_d, B_new_d, B_d, cuda_ret);
+    	update_weight_h[0] = (OUT_h[0] - 1.0f) * 0.05f;
+ 	back_prop_output(OW_new_d, C_d, //, update_weight_d, OW_t_d, cuda_ret);
+	back_prop_hidden(B_d, update_weight_d, A_d, OW_new_d, OW_t_d, B_new_d, B_temp_d, cuda_ret);
+    }
+*/
+    cudaMemcpy(OW_new_h, OW_new_d, sizeof(float)*OW_new_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(OW_h, OW_d, sizeof(float)*OW_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(OW_t_h, OW_t_d, sizeof(float)*OW_t_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(B_new_h, B_new_d, sizeof(float)*B_new_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(update_weight_h, update_weight_d, sizeof(float)*update_sz, cudaMemcpyDeviceToHost);	
+    cudaMemcpy(B_h, B_d, sizeof(float)*B_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(B_temp_h, B_temp_d, sizeof(float)*B_temp_sz, cudaMemcpyDeviceToHost);
+    cudaMemcpy(C_h, C_d, sizeof(float)*C_sz, cudaMemcpyDeviceToHost);
+	
+    printf("\nC: %f/%f/%f/%f/%f", C_h[0],C_h[1],C_h[2],C_h[3],C_h[4]);
+    printf("\nOW_new: %f/%f/%f/%f/%f", OW_new_h[0],OW_new_h[1],OW_new_h[2],OW_new_h[3],OW_new_h[4]);
+    printf("\nOW: %f/%f/%f/%f/%f ", OW_h[0],OW_h[1],OW_h[2],OW_h[3],OW_h[4]);
+    for(int i = 0; i < 20; i++) {
+	printf("\nA: %f/ B_new: %f ", A_h[i], B_new_h[i]);
+    }	
+    printf("\nupdate_weight_h: %f/  \n", update_weight_h[0]);
+    printf("B: %f/%f/%f/%f/%f \n", B_h[0],B_h[1],B_h[2],B_h[3]);
+    printf("B_temp: %f/%f/%f/%f/%f \n", B_temp_h[0],B_temp_h[1],B_temp_h[2],B_temp_h[3]);
     // Verify correctness -----------------------------------------------------
-
     printf("Verifying results..."); fflush(stdout);
 	std::cout << C_sz;
-    verify(A_h, B_h, C_h, OUT_h, B_new_h, matArow, matAcol, matBcol);
-
-
+    verify(A_h, B_h, C_h, OUT_h, matArow, matAcol, matBcol);
     // Free memory ------------------------------------------------------------
-
     free(A_h);
     free(B_h);
     free(C_h);
     free(OW_h);
     free(OUT_h);
-	free(Csig_h);	
-	free(OW_new_h);
-	free(update_weight_h);
-	free(B_new_h);
-	free(B_temp_h);
-	free(OW_t_h);
-
+    free(Csig_h);
+    free(OW_new_h);
+    free(update_weight_h);
+    free(B_new_h);
+    free(B_temp_h);
+    free(OW_t_h);
     /*************************************************************************/
-    //INSERT CODE HERE
-	cudaFree(A_d);
-	cudaFree(B_d);
-	cudaFree(C_d);    
-	cudaFree(OW_d);
-	cudaFree(OUT_d);
-	cudaFree(Csig_d);
-	cudaFree(OW_new_d);
-	cudaFree(update_weight_d);
-	cudaFree(B_new_d);
-	cudaFree(B_temp_d);
-	cudaFree(OW_t_d);
+    //CUDA Free
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);    
+    cudaFree(OW_d);
+    cudaFree(OUT_d);
+    cudaFree(Csig_d);
+    cudaFree(OW_new_d);
+    cudaFree(update_weight_d);
+    cudaFree(B_new_d);
+    cudaFree(B_temp_d);
+    cudaFree(OW_t_d);
     /*************************************************************************/
-
     return 0;
 }
-
